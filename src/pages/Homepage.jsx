b0632acf91c73b0f9580fb2f1358add72ebef0ce
Atilas <vampire91145@gmail.com>
2024-01-26 21:30:30 +0800

    Using CSS modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>World Wise</h1>
+      <Link to="/app">go to the app</Link>
     </div>
   );
 }
