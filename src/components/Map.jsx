3eb2fd811226f3f5976dfd9dd89299d653451c68
Atilas <vampire91145@gmail.com>
2024-04-30 00:29:50 +0800

    Create CitiesContext

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -16,7 +16,12 @@ function Map() {
       <h1>
         Position: {lat}, {lng}
       </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
         Change pos
       </button>
     </div>
