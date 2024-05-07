f570bb7df2924c923356347230213e88193310b0
buo2107 <vampire91145@gmail.com>
2024-05-07 22:29:51 +0800

    Interact with Map: move the Map around as clicking on different cities & show the form as clicking on Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,43 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const { cities } = useCities();
 
   // A bit similar to useState hook
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  // While mapLat and mapLng changes over time, the mapPositon also need to be changed.
+  useEffect(
+    function () {
+      // if mapLat or mapLng not exist, mapPostion maintain the previous value
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
+        // PROBLEM: While position changes, the map won't move there. ("center" is not reactive)
+        // So, we need to implement this functionality on our own within the Leaflet Library
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
@@ -37,9 +54,29 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+// CUTSOM HOOK FOR CHANGE THE MAP CENTER
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+// CUSTOM HOOK FOR CLICKING ON MAP (Because "onClick" doesn't work on Leaflet Library)
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvent({
+    click: (e) => {
+      // console.log(e);
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
